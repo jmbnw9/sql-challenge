68faba0fa74940ccd30c22fd6e4b908001ce90a0
Janelle <jmbnw9@gmail.com>
2021-01-12 21:28:42 -0600

    updating files

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -40,3 +40,7 @@ WHERE d.dept_name = 'Sales';
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY "frequency" DESC;

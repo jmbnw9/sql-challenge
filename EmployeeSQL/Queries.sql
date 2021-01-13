a32c78b3c05db21079493ab8f26efd5f4ac3f558
Janelle <jmbnw9@gmail.com>
2021-01-12 20:54:04 -0600

    updating files

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -24,3 +24,14 @@ FROM dept_emp AS dept
 INNER JOIN employees AS e ON dept.emp_no=e.emp_no
 INNER JOIN departments AS d ON d.dept_no=dept.dept_no;
 
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%'
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
